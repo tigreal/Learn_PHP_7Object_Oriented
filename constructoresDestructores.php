4b1fb82c77727172a9f294cdfc82dd2184802972
tigreal <fosmaralvarez@gmail.com>
2021-02-23 13:05:21 -0400

    Add_contructores_desctructores

---
+++ constructoresDestructores.php
@@ -0,0 +1,30 @@
+<?php
+
+class construcorDestructor
+{
+    public $val1;
+    public $val2;
+    /*el contrutor no puede tener un retorno 
+    este tipo de clases son especiales las de doble
+    guion bajo */
+    public function __construct($var, $var2){
+        $this->val1=$var;
+        $this->val2=$var2;
+
+        
+
+    
+    }
+    /* un destructor no puede llevar argumentos*/
+    public function __destruct()
+    {
+        //se coloca el objto a destruir
+    }
+    
+    
+}
+
+$obj = new construcorDestructor("hola","mundo");
+
+
+?>
