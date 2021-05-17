6ee7ad5cfd0f4787095809fdd6df215019d6a186
tigreal <fosmaralvarez@gmail.com>
2021-05-16 23:04:41 -0400

    empezando los espacios de nombre

---
+++ spaceName/EspaciosDeNombreNameSpaces.php
@@ -0,0 +1,18 @@
+<?php
+// los nombres de espacio solucionan el problema de usar clases con el mismo nombre como 
+// cuando usan clases de terceros, para evitar conflicto de nombres
+// hay limites sobre lo que puedes colocar en un espacio de nombre:
+// • Clases
+// • Interfaces
+// • Funciones
+// • Constantes
+// para utilizar los espacios de nombre una buena practica es usar un nuevo archivo
+// donde estaran solo los espacios de nombre
+// se utiliza la palabra key spacenam+un identificador 
+// en un archivo php esta debe ser la primera linea no puede aver ni html antes de este
+// despues de definir el espacio de nombre se puede debe colocar todo el codigo dentro
+// tambien se puede tener subespacios de nombre se utiliza un backslash /
+// el archivo de espacio de nombre se anade utilizando requier('');
+
+
+?>
